17fd5041ecacdeb204868997cf70279d9bc3a20e
bredele <olivier.wietrich@gmail.com>
2013-10-29 15:42:16 -0600

    add local component props because the component.json raw doesn/'t work

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
+var props = require('lib/props');
 
 /**
  * Expose 'with'
  */
 
 module.exports = function(statement){
-	
+	var result = props(statement, '.model');
+	return new Function('model', result);
 };
